56de9615d27cb66c3e22d728ce2ef14b9e8e74c2
duyphaphach <you@example.com>
2018-07-07 23:13:54 +0700

    Add some fields to feature

--- bigfeature.js
+++ bigfeature.js
@@ -1,3 +1,61 @@
 <html>
 	This is a big feature, treat it right way
 </html>
+
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+
+Additional fields
+Additional fields
+
+Additional fields
+
+Additional fields
+Additional fields
+
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+Additional fields
+
+Additional fields
+Additional fieldsAdditional fieldsAdditional fields
+
+Additional fields
+
+Additional fields
+Additional fields
+
+Additional fields
+Additional fields
+
+
+
+
+
+
+
+
